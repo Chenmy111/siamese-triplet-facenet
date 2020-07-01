8d1165f8303334a34934446e951fb5faba7c51c5
Chenmy111 <cmy11280505@gmail.com>
2020-07-01 08:06:25 +0800

    修改trainer里scheduler.step()的位置

--- run.py
+++ run.py
@@ -12,8 +12,7 @@ import collections
 from networks import EmbeddingNet, SiameseNet
 from losses import ContrastiveLoss
 from utils import Scale
-
-
+from logger import Logger
 
 
 transform = transforms.Compose([
@@ -29,18 +28,6 @@ test_path = './data/val'
 batch_size = 8
 embedding_size = 16
 margin = 1.
-
-siamese_train_dataset = SiameseFaceDataset(dir=train_path, transform=transform)
-siamese_test_dataset = SiameseFaceDataset(dir=test_path, transform=transform, train=False)
-
-
-
-siamese_train_loader = torch.utils.data.DataLoader(siamese_train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
-siamese_test_loader = torch.utils.data.DataLoader(siamese_test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
-
-
-
-
 embedding_net = EmbeddingNet(embedding_size=embedding_size)
 model = SiameseNet(embedding_net)
 if cuda:
@@ -51,5 +38,18 @@ optimizer = optim.Adam(model.parameters(), lr=lr)
 scheduler = lr_scheduler.StepLR(optimizer, 8, gamma=0.1, last_epoch=-1)
 n_epochs = 10
 log_interval = 100
+log_dir = './log'
+optimizer_name = 'Adam'
+LOG_DIR =log_dir + '/run-optim{}-lr{}-embbeding_size{}'.format(optimizer_name, lr, embedding_size)
+logger = Logger(LOG_DIR)
 
-fit(siamese_train_loader, siamese_test_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval)
+
+train_dataset = SiameseFaceDataset(dir=train_path, transform=transform)
+train_dataset = SiameseFaceDataset(dir=test_path, transform=transform, train=False)
+
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False, **kwargs)
+
+
+fit(train_loader, test_loader, model, logger, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval)
--- trainer.py
+++ trainer.py
@@ -3,8 +3,7 @@ import numpy as np
 from tqdm import tqdm
 
 
-
-def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, metrics=[],
+def fit(train_loader, val_loader, model, logger, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, metrics=[],
         start_epoch=0):
     """
     Loaders, model, loss function and metrics should work together for a given task,
@@ -20,20 +19,23 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
 
     for epoch in range(start_epoch, n_epochs):
 
-
         # Train stage
         train_loss, metrics = train_epoch(train_loader, model, epoch, n_epochs, loss_fn, optimizer, cuda, log_interval, metrics)
         scheduler.step()
         message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, train_loss)
+        logger.log_value('Train set: Average loss', train_loss)
         for metric in metrics:
+            logger.log_value(metric.name(), metric.value())
             message += '\t{}: {}'.format(metric.name(), metric.value())
 
         val_loss, metrics = test_epoch(val_loader, model, loss_fn, cuda, metrics)
         val_loss /= len(val_loader)
 
-        message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs,
+        message += '\nEpoch: {}/{}. Test set: Average loss: {:.4f}'.format(epoch + 1, n_epochs,
                                                                                  val_loss)
+        logger.log_value('Test set: Average loss', val_loss)
         for metric in metrics:
+            logger.log_value(metric.name(), metric.value())
             message += '\t{}: {}'.format(metric.name(), metric.value())
 
         print(message)
@@ -93,10 +95,9 @@ def train_epoch(train_loader, model, epoch, n_epochs, loss_fn, optimizer, cuda, 
             #
             # print(message)
 
-            print_message = 'Train Epoch: {}/{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t{}: {}'.format(
+            print_message = 'Train Epoch: {}/{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch + 1, n_epochs, batch_idx * len(data[0]), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), np.mean(losses),
-                metric.name(), metric.value()
+                100. * batch_idx / len(train_loader), np.mean(losses)
             )
             for metric in metrics:
                 print_message += '\t{}: {}'.format(metric.name(), metric.value())
